6d3ace024ef787e6bd67502f0b015656680e2400
Cavvka Cavka <aleksandrag132@gmail.com>
2024-12-11 13:13:43 +0000

    small changes

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -7,7 +7,7 @@ fn calculate(a: i32, b: i32, operator: String) -> String {
     let result = match operator.as_str() {
         "+" => Some(a + b),
         "-" => Some(a - b),
-        "" => Some(a b),
+        "*" => Some(a * b),
         "/" => if b != 0 { Some(a / b) } else { None },
         "%" => if b != 0 { Some(a % b) } else { None },
         _=> None,
